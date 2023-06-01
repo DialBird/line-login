6cd8fee1c4f29e72b63e4402a2cd1e4aef59a1f1
Keisuke Taniguchi <rememberthatk@icloud.com>
2023-06-01 22:38:56 +0900

    s

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -4,7 +4,7 @@ import SigninButton from "./SigninButton";
 
 const AppBar = () => {
   return (
-    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-300 shadow">
       <Link className="transition-colors hover:text-blue-500" href={"/"}>
         Home Page
       </Link>
